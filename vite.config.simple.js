e52c9e0c6697f054d6120ce761dd0e7051dd1ef8
martijn-atakumi <martijn@atakumi.com>
2025-09-03 17:36:34 +1200

    feat: update build:bundle script to include config and add vite configuration file

---
+++ vite.config.simple.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'AtakumiChat',
+      formats: ['es', 'umd'],
+      fileName: (format) => `chat.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
